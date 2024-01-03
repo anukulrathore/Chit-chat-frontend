86f8d4093164c4823ba3da647e59685206ed9d3c
Rathore <anukul.rathore@accenture.com>
2024-01-03 22:34:06 +0530

    alt warning fixed

--- src/components/Join/Join.jsx
+++ src/components/Join/Join.jsx
@@ -18,7 +18,7 @@ const Join = () => {
     <div className='JoinPage'>
         <div className='JoinContainer'>
             <h1>Chit Chat</h1>
-            <img src={logo} ></img>
+            <img src={logo} alt=''></img>
             <input type="text" placeholder='Enter Your name' id='inputfield'/>
             <Link to='/chat'><button onClick={setUser} type='submit' className='button'>Login</button></Link>
             
